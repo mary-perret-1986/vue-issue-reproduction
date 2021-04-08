45231cf716fb4acda8d447fccedf94e9fd6043af
Mary Perret <mary.perret.1986@gmail.com>
2021-04-08 22:28:20 +0200

    feat(repro): fix typo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="@/assets/logo.png'" />
+    <img alt="Vue logo" src="@/assets/logo.png" />
     <Counter msg="Counter" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
